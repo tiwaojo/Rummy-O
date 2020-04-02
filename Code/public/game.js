777de5cbbc10a4ad2539e638543d295a0647b26e
tiwaojo <tiwaojo@gmail.com>
2020-04-02 18:02:12 -0400

    Index.html created and assets added. Work on game.html has begun

--- Code/public/game.js
+++ Code/public/game.js
@@ -2,3 +2,14 @@ var socket = io();
 socket.on('message', function(data) {
     console.log(data);
 });
+
+function exitGame() {
+    alert("Exit: Exiting the game will close the game and give the other player the glory");
+}
+// function on() {
+//     document.getElementById("nameOverlay").style.display = "none";
+// }
+
+// function off() {
+//     document.getElementById("nameOverlay").style.display = "block";
+// }
