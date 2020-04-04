b7c4963284c4765b9ba4fb86f0177996b95e153b
tiwaojo <tiwaojo@gmail.com>
2020-04-03 22:34:31 -0400

    User name is a required input. Font changed

--- Code/public/game.js
+++ Code/public/game.js
@@ -3,6 +3,9 @@ socket.on('message', function(data) {
     console.log(data);
 });
 
+var playerName = prompt("Please Insert a name: ");
+alert("Welcome: " + playerName);
+
 function exitGame() {
     alert("Exit: Exiting the game will close the game and give the other player the glory");
 }
