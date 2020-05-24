5dd9e8937953407975e7bcba2492d2eed3d3c967
Tiwaloluwa Ojo <100700622@oncampus.local>
2020-05-24 15:58:38 -0400

    Namespace and unique rooms are created. Rough Table for players points are created

--- Code/public/game.js
+++ Code/public/game.js
@@ -1,14 +1,60 @@
-var socket = io();
-socket.on('message', function(data) {
+//'Emit' to send data, 'on' to receive data
+
+//Client side connection to tell Socket.IO client to connect to namespace room
+var game = io('/room');
+game.on('message', function (data) {
     console.log(data);
 });
 
-var playerName = prompt("Please Insert a name: ");
+// Copied and pasted same random num generator. Couldn't figure out how to create global variables
+var roomVal=Math.floor(Math.random()*3);
+console.log("This player is on room "+roomVal);
+//emits room message to the server indicating the player intends to join the room specified
+game.emit("joinRoom","room"+roomVal);
+game.emit("name",playerName);
+
+// Logs a message if the player fails to enter the room
+game.on("err",function(err){
+console.log(err);
+});
+// Logs a success message if the player connects to the right room
+game.on("Success",function(res){
+console.log(res);
+});
+
+//This code is intended to send clients name to server
+game.on("playerNames",function(res){
+console.log(res);
+});
+
+
+var playerName;
+function name() {
+    playerName = prompt("Please Insert a name: ");
+    if (playerName == null || playerName=="" || playerName.startsWith(" ") || playerName.endsWith(" ") || playerName=="null") {
+        name();       
+    }
+     console.log(playerName);
+    //  return playerName;
+}
 alert("Welcome: " + playerName);
+
+function displayName(){
+    // playerName;
+    document.write("<th>"+playerName+"</th>")
+}
+
+// game.emit("player",playerName);
+// sending to all clients in 'game1' and/or in 'game2' room, except sender
+game.on('room').emit('name', playerName);
+
 
 function exitGame() {
     alert("Exit: Exiting the game will close the game and give the other player the glory");
 }
+
+
+
 // function on() {
 //     document.getElementById("nameOverlay").style.display = "none";
 // }
--- Code/server.js
+++ Code/server.js
@@ -14,7 +14,12 @@ app.get('/', function(request, response) {
 });
 // app.use(express.static("images"));
 // Add the WebSocket handlers 
-io.on('connection', function(socket) {}); 
+io.on('connection', function(socket) {
+    
+}); 
+
+
+
 setInterval(function() {
     io.sockets.emit('message', 'hi!');
 }, 1000);
@@ -22,6 +27,58 @@ setInterval(function() {
 server.listen(5000, function() {
     console.log('Starting server on port 5000');
 });
+
+
+//Initializing game room and namespace. 
+const gameNamespace=io.of("/room");
+
+//Random number is generated for the unique room. Game room is generated
+var roomVal=Math.floor(Math.random()*3);
+const gameRoom = "room"+roomVal;
+
+//generating game room namespace. 
+gameNamespace.on('connection',function(socket){
+
+    gameNamespace.on('name', function(data){
+    console.log("Name : " + data);
+    });
+
+    //Logs on the terminal that the clients have joined both the namespace and the room
+console.log("Someone has joined Namespace");
+console.log("Players are on room"+gameRoom);
+gameNamespace.emit("message","hi everyone");
+
+//checks if the room exists join the room. Else return an error message the room doesn't exist
+if(gameRoom.includes(gameRoom)){
+    socket.join(gameRoom);
+    return socket.emit("Success","You have successfully joined this room"+gameRoom);
+}else{
+    return socket.emit("err","ERROR, No Room Named"+gameRoom);
+}
+
+
+
+// game.disconnect();
+});
+// socket.disconnect();
+
+io.of("/room").in("name",function(socket){
+    socket.on("new player",function(name){
+console.log(name);
+    })
+})
+
+io.of('/room').on('name',function(res){
+    io.in('game').emit('playerNames', res);
+    // console.log(res);
+    });
+
+// io.sockets.on('connection', function (client)
+// socket.on("player",function(playerName){
+//     console.log(playerName);
+// });
+
+
 //io.on('connection', function(socket) {
 // socket.on('new player', function() {
 //  player[socket.id] = {
