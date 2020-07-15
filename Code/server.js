673435255bca9ceffbe0b67a00b36585410299a9
100707721 <62113312+100707721@users.noreply.github.com>
2020-07-15 17:50:41 -0400

    Update server.js

--- Code/server.js
+++ Code/server.js
@@ -16,62 +16,63 @@ app.get('/', function (request, response) {
 // Add the WebSocket handlers 
 
 //Random number is generated for the unique room. Game room is generated
-var roomVal = Math.floor(Math.random() * 103);
+// var roomVal = Math.floor(Math.random() * 103);
 
-var gameRoom = "room" + roomVal;
+// var gameRoom = "room" + roomVal;
 // io.in("roomVal"+roomVal).emit("roomVal",roomVal);
 
-//On connection to the socket, the server terminal logs a message 'a user connected. [gameRoom] initiated for all clients'
-io.on('connection', function (socket) {
-
-    //a message is logged on the server console with the game room for the client
-    console.log('a user connected. ' + gameRoom + " initiated for all clients");
-    
-    //sends roomVal to player to connect to room
-socket.emit("roomVal",roomVal);
-    
-    //if the clients room is the same as the servers then the client can join, else an error message is sent
-    socket.on("joinRoom", function (clientRoom) {
-        console.log("Client is on" + clientRoom);
-        //if the clients room is the same as the servers then client can join else error message is sent
-        if (clientRoom == gameRoom) {
-            socket.join(gameRoom);
-console.log("Player has successfully joined " + gameRoom);
-            return socket.emit("Success", "Player has successfully joined " + gameRoom);
-        } else {
-            return socket.emit("err", "ERROR, No Room Named " + gameRoom);
-        }
-    });
-
-    //if uniqueRoom (the clients intended room) != the auto generated room, the gameRoom is refactored to clients uniqueRoom.
-    //the server reconnects to the clients intended room else return error
-    socket.on("uniqueRoom", function (uniqueRoom) {
-        if (uniqueRoom != roomVal) {
-
-            gameRoom = "room" + uniqueRoom;
-            socket.join(gameRoom);
-            return socket.emit("Success", "Player has successfully joined " + gameRoom);
-        }
-        else {
-            return socket.emit("err", "ERROR, No Room Named " + gameRoom);
-        }
-
-    });
-
-    //Notifies if the client has disconnected from the socket. i.e. closed the tab
-    socket.on('disconnect', () => {
-        console.log('user disconnected');
-    });
-
+io.on('connection', (socket) => {
     socket.on('chat message', (msg) => {
         io.emit('chat message', msg);
-      });
-
-    // io.emit('some event', { someProperty: 'some value', otherProperty: 'other value' }); // This will emit the event to all connected sockets
-
+    });
 });
+//On connection to the socket, the server terminal logs a message 'a user connected. [gameRoom] initiated for all clients'
+// io.on('connection', function (socket) {
 
+//     //a message is logged on the server console with the game room for the client
+//     console.log('a user connected. ' + gameRoom + " initiated for all clients");
 
+//     //sends roomVal to player to connect to room
+//     socket.emit("roomVal", roomVal);
+
+//     //if the clients room is the same as the servers then the client can join, else an error message is sent
+//     socket.on("joinRoom", function (clientRoom) {
+//         console.log("Client is on" + clientRoom);
+//         //if the clients room is the same as the servers then client can join else error message is sent
+//         if (clientRoom == gameRoom) {
+//             socket.join(gameRoom);
+//             console.log("Player has successfully joined " + gameRoom);
+//             return socket.emit("Success", "Player has successfully joined " + gameRoom);
+//         } else {
+//             return socket.emit("err", "ERROR, No Room Named " + gameRoom);
+//         }
+//     });
+
+//     //if uniqueRoom (the clients intended room) != the auto generated room, the gameRoom is refactored to clients uniqueRoom.
+//     //the server reconnects to the clients intended room else return error
+//     socket.on("uniqueRoom", function (uniqueRoom) {
+//         if (uniqueRoom != roomVal) {
+
+//             gameRoom = "room" + uniqueRoom;
+//             socket.join(gameRoom);
+//             return socket.emit("Success", "Player has successfully joined " + gameRoom);
+//         }
+//         else {
+//             return socket.emit("err", "ERROR, No Room Named " + gameRoom);
+//         }
+
+//     });
+
+//     //Notifies if the client has disconnected from the socket. i.e. closed the tab
+//     socket.on('disconnect', () => {
+//         console.log('user disconnected');
+//     });
+
+//     io.emit('some event', { someProperty: 'some value', otherProperty: 'other value' }); // This will emit the event to all connected sockets
+
+// });
+
+//Use this to syncronize the changes for the cards in hand
 // setInterval(function () {
 //     io.sockets.emit('message', 'hi!');
 // }, 1000);
@@ -129,7 +130,7 @@ io.of("/room").in("name", function (socket) {
     socket.on("new player", function (name) {
         console.log(name);
     })
-})
+});
 
 io.of('/room').on('name', function (res) {
     io.in('game').emit('playerNames', res);
